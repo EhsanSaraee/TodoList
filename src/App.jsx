1cb224de2062f88fc4c3ca0d9691236317f4fe64
ehsan <ehsansaraee6570@yahoo.com>
2021-11-27 11:10:10 +0330

    initialize project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './app.css';
+import TodoApp from './Components/TodoApp';
+
+const App = () => {
+   return (
+      <div className="App">
+         <h1>TodoList App - Ehsan</h1>
+         <TodoApp />
+      </div>
+   );
+};
+
+export default App;
